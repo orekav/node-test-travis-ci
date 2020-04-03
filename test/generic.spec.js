b0545d569115093d1047b7d16de4ce8415c48aea
Nicolas Vaquero <nvaquero@est.frba.utn.edu.ar>
2020-04-03 20:57:33 +0200

    Fixes test type error

--- test/generic.spec.js
+++ test/generic.spec.js
@@ -1,6 +1,6 @@
 describe("Dummy test suit", function () {
 	it("Should be in PORT 8080", function (done) {
-		if (process.env.PORT === 8080)
+		if (process.env.PORT === "8080")
 			done();
 		else
 			throw new Error(`The port was ${process.env.PORT}`);
