2e93e8036a7f158f23bc8df1907af4d8d44c2a8c
Nicolas Vaquero <nvaquero@est.frba.utn.edu.ar>
2020-04-04 20:48:58 +0200

    Splits server listening to server.js file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,5 @@ const express = require("express");
 const app = express();
 
 app.all("*", (req, res) => res.json({ message: "All is well" }));
-app.listen(process.env.PORT);
 
 module.exports = app;
