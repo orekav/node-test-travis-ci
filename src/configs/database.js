a8f0f894a8bbc995de8aa5c9c98be6b554d5ab90
Nicolas Alejandro Vaquero <nvaquero@est.frba.utn.edu.ar>
2020-05-26 15:00:53 +0200

    Updates database config file

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,9 +2,23 @@ const env = process.env.NODE_ENV || "development";
 
 const config = {
 	development: {
-		dialect: "sqlite",
-		storage: "./database.sqlite3",
+		username: process.env.DATABASE_USERNAME,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE_DATABASENAME,
+		host: process.env.DATABASE_HOST,
 		logging: false,
+		pool: {
+			max: 50,
+			min: 1,
+			idle: 30000
+		},
+		dialectOptions: {
+			useUTC: false,
+			encrypt: true,
+			requestTimeout: 30000
+		},
+		dialect: "mariadb",
+		timezone: "GMT",
 	},
 	test: {
 		dialect: "sqlite",
@@ -27,7 +41,8 @@ const config = {
 			encrypt: true,
 			requestTimeout: 30000
 		},
-		dialect: "mariadb"
+		dialect: "mariadb",
+		timezone: "GMT",
 	}
 };
 
