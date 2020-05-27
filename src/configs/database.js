bf17148492067a1fa9b30e13d2ca2e8cc4d9ff93
Nicolas Alejandro Vaquero <nvaquero@est.frba.utn.edu.ar>
2020-05-27 19:54:29 +0200

    Updates travis.yml and database config file

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,5 +1,42 @@
 const env = process.env.NODE_ENV || "development";
 
+const databaseConnection = {
+	dialect: process.env.DATABASE_DIALECT,
+	username: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	database: process.env.DATABASE_DATABASENAME,
+};
+
+const sslConfig = () => {
+	if (process.env.DATABASE_USE_SSL !== "false")
+		return { required: true, rejectUnauthorized: false };
+	else
+		return null;
+};
+
+const poolConfig = {
+	max: 10,
+	min: 1,
+	idle: 30000
+};
+
+const dialectOptions = {
+	useUTC: false,
+	encrypt: true,
+	requestTimeout: 30000,
+	ssl: sslConfig(),
+};
+
+const config = {
+	...databaseConnection,
+	pool: poolConfig,
+	dialectOptions: dialectOptions,
+	timezone: "GMT",
+	logging: false,
+};
+
 const options = {
 	test: {
 		dialect: "sqlite",
@@ -7,52 +44,10 @@ const options = {
 		logging: false,
 	},
 	development: {
-		dialect: process.env.DATABASE_DIALECT,
-		username: process.env.DATABASE_USERNAME,
-		password: process.env.DATABASE_PASSWORD,
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT,
-		database: process.env.DATABASE_DATABASENAME,
-		logging: false,
-		pool: {
-			max: 10,
-			min: 1,
-			idle: 30000
-		},
-		dialectOptions: {
-			useUTC: false,
-			encrypt: true,
-			requestTimeout: 30000,
-			ssl: {
-				required: true,
-				rejectUnauthorized: false,
-			},
-		},
-		timezone: "GMT",
+		...config,
 	},
 	production: {
-		dialect: process.env.DATABASE_DIALECT,
-		username: process.env.DATABASE_USERNAME,
-		password: process.env.DATABASE_PASSWORD,
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT,
-		database: process.env.DATABASE_DATABASENAME,
-		logging: false,
-		pool: {
-			max: 10,
-			min: 1,
-			idle: 30000
-		},
-		dialectOptions: {
-			useUTC: false,
-			encrypt: true,
-			requestTimeout: 30000,
-			ssl: {
-				required: true,
-				rejectUnauthorized: false,
-			},
-		},
-		timezone: "GMT",
+		...config,
 	}
 };
 
